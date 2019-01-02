268a6f96949ddcb2bce3c59ea66b8c699a6dcfd4
ntr <ntr@ihfood.dk>
2019-01-02 15:29:30 +0100

    Add ReadOnlyView0D

---
+++ src/DotNetCross.Memory.Views/ReadOnlyView0D.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace DotNetCross.Memory.Views
+{
+    public readonly struct ReadOnlyView0D<T>
+    {
+        readonly object _objectOrNull;
+        readonly IntPtr _byteOffsetOrPointer;
+
+        public ReadOnlyView0D(T[] array, int index)
+        {
+            if (array == null)
+                ThrowHelper.ThrowArgumentNullException(ExceptionArgument.array);
+            if (default(T) == null && array.GetType() != typeof(T[]))
+                ThrowHelper.ThrowArrayTypeMismatchException_ArrayTypeMustBeExactMatch(typeof(T));
+
+            int arrayLength = array.Length;
+            if ((uint)index > (uint)arrayLength)
+                ThrowHelper.ThrowArgumentOutOfRangeException(ExceptionArgument.index);
+
+            _objectOrNull = array;
+            _byteOffsetOrPointer = Unsafe.ByteOffset(_objectOrNull, ref array[index]);
+        }
+
+        public ReadOnlyView0D(object obj, in T member)
+        {
+            _objectOrNull = obj;
+            _byteOffsetOrPointer = Unsafe.ByteOffset(_objectOrNull, ref Unsafe.AsRef<T>(member));
+        }
+
+        public ReadOnlyView0D(IntPtr pointer)
+        {
+            _objectOrNull = null;
+            _byteOffsetOrPointer = pointer;
+        }
+
+        public unsafe ReadOnlyView0D(void* pointer)
+        {
+            _objectOrNull = null;
+            _byteOffsetOrPointer = new IntPtr(pointer);
+        }
+
+        // We cannot return "ref readonly" yet, C# 8?
+        public /*ref readonly*/ T Element => GetPinnableReference();
+
+        // We cannot return "ref readonly" yet, C# 8?
+        public ref /*readonly*/ T GetPinnableReference()
+        {
+            return ref Unsafe.RefAtByteOffset<T>(_objectOrNull, _byteOffsetOrPointer);
+        }
+    }
+}
---
+++ tests/DotNetCross.Memory.Views.Tests/ReadOnlyView0DTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DotNetCross.Memory.Views.Tests
+{
+    [TestClass]
+    public class ReadOnlyView0DTest
+    {
+        [TestMethod]
+        public void ReadOnlyView0DTest_Ctor_ArrayIndex()
+        {
+            var array = new int[] { 17, 18, 19, 20 };
+            var view = new ReadOnlyView0D<int>(array, 2);
+            Assert.AreEqual(19, view.Element);
+        }
+    }
+}
