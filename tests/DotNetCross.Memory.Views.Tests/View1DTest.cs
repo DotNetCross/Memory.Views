38cecb03a22db1ca30ccb7f7ef86c511a436af6e
ntr <ntr@ihfood.dk>
2019-03-16 22:23:11 +0100

    Add View1D AsSpan test for array

--- tests/DotNetCross.Memory.Views.Tests/View1DTest.cs
+++ tests/DotNetCross.Memory.Views.Tests/View1DTest.cs
@@ -15,10 +15,21 @@ namespace DotNetCross.Memory.Views.Tests
             ref int v1 = ref view[1];
             Assert.Equal(18, v0);
             Assert.Equal(19, v1);
-#if HASSPAN
             var span = view.AsSpan();
             ref int s0 = ref span[0];
-#endif
         }
+
+#if HASSPAN
+        [Fact]
+        public void View1DTest_AsSpan_Array()
+        {
+            var array = new int[] { 17, 18, 19, 20 };
+            var view = new View1D<int>(array, 1, 2);
+            var span = view.AsSpan();
+            Assert.Equal(2, span.Length);
+            Assert.Equal(18, span[0]);
+            Assert.Equal(19, span[1]);
+        }
+#endif
     }
 }
