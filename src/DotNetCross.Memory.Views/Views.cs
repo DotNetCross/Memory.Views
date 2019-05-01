a24e7c2a8173532d1046cf33ec5591e5839f3fe8
ntr <ntr@ihfood.dk>
2019-05-01 22:25:27 +0200

    code gen views

---
+++ src/DotNetCross.Memory.Views/Views.cs
@@ -0,0 +1,24 @@
+﻿ 
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace IHFood.Numerics
+{
+    public readonly struct View1D
+    {
+    }
+    public readonly struct View2D
+    {
+    }
+    public readonly struct View3D
+    {
+    }
+    public readonly struct View4D
+    {
+    }
+    public readonly struct View5D
+    {
+    }
+}
