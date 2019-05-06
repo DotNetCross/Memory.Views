ea403ec96852f88a58f47810963a1453f4c60855
ntr <ntr@ihfood.dk>
2019-05-06 20:59:09 +0200

    start on ctor by array

--- src/DotNetCross.Memory.Views/Views.cs
+++ src/DotNetCross.Memory.Views/Views.cs
@@ -6,15 +6,20 @@ using System.Runtime.CompilerServices;
 
 namespace IHFood.Numerics
 {
-    public readonly struct View1D
+    public readonly struct View1D<T>
     {
         readonly object _objectOrNull;
         readonly IntPtr _byteOffsetOrPointer;
         // .NET has for good chosen int as size, would have preferred IntPtr e.g. nint
         // for length but this would give issues with interoperating with BCL
         readonly int _length0;
+
+        public View1D(T[] array)
+        {
+            
+        }
     }
-    public readonly struct View2D
+    public readonly struct View2D<T>
     {
         readonly object _objectOrNull;
         readonly IntPtr _byteOffsetOrPointer;
@@ -23,8 +28,13 @@ namespace IHFood.Numerics
         // for length but this would give issues with interoperating with BCL
         readonly int _length0;
         readonly int _length1;
+
+        public View2D(T[,] array)
+        {
+            
+        }
     }
-    public readonly struct View3D
+    public readonly struct View3D<T>
     {
         readonly object _objectOrNull;
         readonly IntPtr _byteOffsetOrPointer;
@@ -35,8 +45,13 @@ namespace IHFood.Numerics
         readonly int _length0;
         readonly int _length1;
         readonly int _length2;
+
+        public View3D(T[,,] array)
+        {
+            
+        }
     }
-    public readonly struct View4D
+    public readonly struct View4D<T>
     {
         readonly object _objectOrNull;
         readonly IntPtr _byteOffsetOrPointer;
@@ -49,8 +64,13 @@ namespace IHFood.Numerics
         readonly int _length1;
         readonly int _length2;
         readonly int _length3;
+
+        public View4D(T[,,,] array)
+        {
+            
+        }
     }
-    public readonly struct View5D
+    public readonly struct View5D<T>
     {
         readonly object _objectOrNull;
         readonly IntPtr _byteOffsetOrPointer;
@@ -65,5 +85,10 @@ namespace IHFood.Numerics
         readonly int _length2;
         readonly int _length3;
         readonly int _length4;
+
+        public View5D(T[,,,,] array)
+        {
+            
+        }
     }
 }
