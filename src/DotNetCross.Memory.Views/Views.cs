02a6a8dec6d382f60b3db5d7af2212b49d3e40b1
ntr <ntr@ihfood.dk>
2019-09-07 10:28:42 +0200

    ctor with start + lengths

--- src/DotNetCross.Memory.Views/Views.cs
+++ src/DotNetCross.Memory.Views/Views.cs
@@ -44,6 +44,24 @@ namespace DotNetCross.Memory.Views
             _byteOffsetOrPointer = _byteOffsetOrPointer
                 .Add<T>(start0);
         }
+
+        public View1D(T[] array, int start0,
+            int length0)
+        {
+            if (array == null)
+                ThrowHelper.ThrowArgumentNullException(ExceptionArgument.array);
+            if (default(T) == null && array.GetType() != typeof(T[]))
+                ThrowHelper.ThrowArrayTypeMismatchException_ArrayTypeMustBeExactMatch(typeof(T));
+
+            _objectOrNull = array;
+            _byteOffsetOrPointer = ViewHelper.PerTypeValues<T>.ArrayAdjustment1D;
+            _length0 = array.GetLength(0);
+            if ((uint)start0 > (uint)_length0 || (uint)length0 > (uint)(_length0 - start0))
+                ThrowHelper.ThrowArgumentOutOfRangeException(ExceptionArgument.start0);
+            _byteOffsetOrPointer = _byteOffsetOrPointer
+                .Add<T>(start0);
+            _length0 = length0;
+        }
     }
     [StructLayout(LayoutKind.Sequential)]
     public readonly struct View2D<T>
@@ -89,6 +107,30 @@ namespace DotNetCross.Memory.Views
             _byteOffsetOrPointer = _byteOffsetOrPointer
                 .Add(_byteStride0.Multiply(start0))
                 .Add<T>(start1);
+        }
+
+        public View2D(T[,] array, int start0, int start1,
+            int length0, int length1)
+        {
+            if (array == null)
+                ThrowHelper.ThrowArgumentNullException(ExceptionArgument.array);
+            if (default(T) == null && array.GetType() != typeof(T[]))
+                ThrowHelper.ThrowArrayTypeMismatchException_ArrayTypeMustBeExactMatch(typeof(T));
+
+            _objectOrNull = array;
+            _byteOffsetOrPointer = ViewHelper.PerTypeValues<T>.ArrayAdjustment2D;
+            _length0 = array.GetLength(0);
+            _length1 = array.GetLength(1);
+            _byteStride0 = new IntPtr(_length1).Multiply(Unsafe.SizeOf<T>());
+            if ((uint)start0 > (uint)_length0 || (uint)length0 > (uint)(_length0 - start0))
+                ThrowHelper.ThrowArgumentOutOfRangeException(ExceptionArgument.start0);
+            if ((uint)start1 > (uint)_length1 || (uint)length1 > (uint)(_length1 - start1))
+                ThrowHelper.ThrowArgumentOutOfRangeException(ExceptionArgument.start1);
+            _byteOffsetOrPointer = _byteOffsetOrPointer
+                .Add(_byteStride0.Multiply(start0))
+                .Add<T>(start1);
+            _length0 = length0;
+            _length1 = length1;
         }
     }
     [StructLayout(LayoutKind.Sequential)]
@@ -144,6 +186,36 @@ namespace DotNetCross.Memory.Views
                 .Add(_byteStride0.Multiply(start0))
                 .Add(_byteStride1.Multiply(start1))
                 .Add<T>(start2);
+        }
+
+        public View3D(T[,,] array, int start0, int start1, int start2,
+            int length0, int length1, int length2)
+        {
+            if (array == null)
+                ThrowHelper.ThrowArgumentNullException(ExceptionArgument.array);
+            if (default(T) == null && array.GetType() != typeof(T[]))
+                ThrowHelper.ThrowArrayTypeMismatchException_ArrayTypeMustBeExactMatch(typeof(T));
+
+            _objectOrNull = array;
+            _byteOffsetOrPointer = ViewHelper.PerTypeValues<T>.ArrayAdjustment3D;
+            _length0 = array.GetLength(0);
+            _length1 = array.GetLength(1);
+            _length2 = array.GetLength(2);
+            _byteStride1 = new IntPtr(_length2).Multiply(Unsafe.SizeOf<T>());
+            _byteStride0 = _byteStride1.Multiply(_length1);
+            if ((uint)start0 > (uint)_length0 || (uint)length0 > (uint)(_length0 - start0))
+                ThrowHelper.ThrowArgumentOutOfRangeException(ExceptionArgument.start0);
+            if ((uint)start1 > (uint)_length1 || (uint)length1 > (uint)(_length1 - start1))
+                ThrowHelper.ThrowArgumentOutOfRangeException(ExceptionArgument.start1);
+            if ((uint)start2 > (uint)_length2 || (uint)length2 > (uint)(_length2 - start2))
+                ThrowHelper.ThrowArgumentOutOfRangeException(ExceptionArgument.start2);
+            _byteOffsetOrPointer = _byteOffsetOrPointer
+                .Add(_byteStride0.Multiply(start0))
+                .Add(_byteStride1.Multiply(start1))
+                .Add<T>(start2);
+            _length0 = length0;
+            _length1 = length1;
+            _length2 = length2;
         }
     }
     [StructLayout(LayoutKind.Sequential)]
@@ -208,6 +280,42 @@ namespace DotNetCross.Memory.Views
                 .Add(_byteStride1.Multiply(start1))
                 .Add(_byteStride2.Multiply(start2))
                 .Add<T>(start3);
+        }
+
+        public View4D(T[,,,] array, int start0, int start1, int start2, int start3,
+            int length0, int length1, int length2, int length3)
+        {
+            if (array == null)
+                ThrowHelper.ThrowArgumentNullException(ExceptionArgument.array);
+            if (default(T) == null && array.GetType() != typeof(T[]))
+                ThrowHelper.ThrowArrayTypeMismatchException_ArrayTypeMustBeExactMatch(typeof(T));
+
+            _objectOrNull = array;
+            _byteOffsetOrPointer = ViewHelper.PerTypeValues<T>.ArrayAdjustment4D;
+            _length0 = array.GetLength(0);
+            _length1 = array.GetLength(1);
+            _length2 = array.GetLength(2);
+            _length3 = array.GetLength(3);
+            _byteStride2 = new IntPtr(_length3).Multiply(Unsafe.SizeOf<T>());
+            _byteStride1 = _byteStride2.Multiply(_length2);
+            _byteStride0 = _byteStride1.Multiply(_length1);
+            if ((uint)start0 > (uint)_length0 || (uint)length0 > (uint)(_length0 - start0))
+                ThrowHelper.ThrowArgumentOutOfRangeException(ExceptionArgument.start0);
+            if ((uint)start1 > (uint)_length1 || (uint)length1 > (uint)(_length1 - start1))
+                ThrowHelper.ThrowArgumentOutOfRangeException(ExceptionArgument.start1);
+            if ((uint)start2 > (uint)_length2 || (uint)length2 > (uint)(_length2 - start2))
+                ThrowHelper.ThrowArgumentOutOfRangeException(ExceptionArgument.start2);
+            if ((uint)start3 > (uint)_length3 || (uint)length3 > (uint)(_length3 - start3))
+                ThrowHelper.ThrowArgumentOutOfRangeException(ExceptionArgument.start3);
+            _byteOffsetOrPointer = _byteOffsetOrPointer
+                .Add(_byteStride0.Multiply(start0))
+                .Add(_byteStride1.Multiply(start1))
+                .Add(_byteStride2.Multiply(start2))
+                .Add<T>(start3);
+            _length0 = length0;
+            _length1 = length1;
+            _length2 = length2;
+            _length3 = length3;
         }
     }
     [StructLayout(LayoutKind.Sequential)]
@@ -281,6 +389,48 @@ namespace DotNetCross.Memory.Views
                 .Add(_byteStride2.Multiply(start2))
                 .Add(_byteStride3.Multiply(start3))
                 .Add<T>(start4);
+        }
+
+        public View5D(T[,,,,] array, int start0, int start1, int start2, int start3, int start4,
+            int length0, int length1, int length2, int length3, int length4)
+        {
+            if (array == null)
+                ThrowHelper.ThrowArgumentNullException(ExceptionArgument.array);
+            if (default(T) == null && array.GetType() != typeof(T[]))
+                ThrowHelper.ThrowArrayTypeMismatchException_ArrayTypeMustBeExactMatch(typeof(T));
+
+            _objectOrNull = array;
+            _byteOffsetOrPointer = ViewHelper.PerTypeValues<T>.ArrayAdjustment5D;
+            _length0 = array.GetLength(0);
+            _length1 = array.GetLength(1);
+            _length2 = array.GetLength(2);
+            _length3 = array.GetLength(3);
+            _length4 = array.GetLength(4);
+            _byteStride3 = new IntPtr(_length4).Multiply(Unsafe.SizeOf<T>());
+            _byteStride2 = _byteStride3.Multiply(_length3);
+            _byteStride1 = _byteStride2.Multiply(_length2);
+            _byteStride0 = _byteStride1.Multiply(_length1);
+            if ((uint)start0 > (uint)_length0 || (uint)length0 > (uint)(_length0 - start0))
+                ThrowHelper.ThrowArgumentOutOfRangeException(ExceptionArgument.start0);
+            if ((uint)start1 > (uint)_length1 || (uint)length1 > (uint)(_length1 - start1))
+                ThrowHelper.ThrowArgumentOutOfRangeException(ExceptionArgument.start1);
+            if ((uint)start2 > (uint)_length2 || (uint)length2 > (uint)(_length2 - start2))
+                ThrowHelper.ThrowArgumentOutOfRangeException(ExceptionArgument.start2);
+            if ((uint)start3 > (uint)_length3 || (uint)length3 > (uint)(_length3 - start3))
+                ThrowHelper.ThrowArgumentOutOfRangeException(ExceptionArgument.start3);
+            if ((uint)start4 > (uint)_length4 || (uint)length4 > (uint)(_length4 - start4))
+                ThrowHelper.ThrowArgumentOutOfRangeException(ExceptionArgument.start4);
+            _byteOffsetOrPointer = _byteOffsetOrPointer
+                .Add(_byteStride0.Multiply(start0))
+                .Add(_byteStride1.Multiply(start1))
+                .Add(_byteStride2.Multiply(start2))
+                .Add(_byteStride3.Multiply(start3))
+                .Add<T>(start4);
+            _length0 = length0;
+            _length1 = length1;
+            _length2 = length2;
+            _length3 = length3;
+            _length4 = length4;
         }
     }
 }
